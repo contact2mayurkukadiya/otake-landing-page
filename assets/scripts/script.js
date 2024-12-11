33e957fba853ceecb778ed9f42d2cf60c32e49ba
Mayur <contact2mayurkukadiya@gmail.com>
2024-12-11 18:10:12 +0530

    Implement mobile menu toggle functionality in script.js

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+// mobile menu
+
+const mobileMenuButton = document.querySelector('button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
